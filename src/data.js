441a36fd1ef8a46d1a03020e43ee435617de47d8
Marii lopez <lopezmariana997@gmail.com>
2023-02-16 12:38:57 -0500

    Visualización de data

--- src/data.js
+++ src/data.js
@@ -3,13 +3,8 @@ export const pokemon = (tarjetas) => {
   return 'pokemon';
 };
 
-function mostrarTarjetas(tarjetas){
-let number = [pokemon];
-for(let i = 0; i < number.length; i++){
-return number;
-}
-};
-console.log(mostrarTarjetas);
+
+
 
 export const anotherExample = () => {
   return 'OMG';
--- src/main.js
+++ src/main.js
@@ -1,10 +1,32 @@
-import { pokemon } from './data.js';
+
+
 //No lo podemos borrar
-import data from './data/pokemon/pokemon.js'; 
+import datos from './data/pokemon/pokemon.js'; 
+const arregloPokemon = datos.pokemon.slice(0,18)
 
+const vistaPokemon = document.getElementById('vistaPokemon')
+ function mostrarTarjetas(arregloPokemon){
+    console.log(arregloPokemon);
+    for(let i = 0; i < arregloPokemon.length; i++){
+     vistaPokemon.innerHTML += `<article>
+     <h1>${arregloPokemon[i].name}</h1> 
+     <h2>Tipo:</h2>        
+     <h3>${arregloPokemon[i].type}<h3/>
+     <img src="${arregloPokemon[i].img}" alt="">
+     <h4>Resistencia:</h4>
+     <h5>${arregloPokemon[i].resistant}</h5>
+     <h6>${arregloPokemon[i].num}</h6>
+   </article>` // sugar sintax
 
-console.log(pokemon, data);
-document.getElementById("root").innerHTML = pokemon; 
+     /*  const h1 = document.createElement('h1')
+       h1.textContent = arregloPokemon[i].name
+       vistaPokemon.appendChild(h1)*/
+    }
+    
+    };
+
+mostrarTarjetas(arregloPokemon)
+//document.getElementById("root").innerHTML = pokemon; 
 
 //const jsonPokemon=JSON.parse(name);
 //console.log(typeof jsonPokemon);
