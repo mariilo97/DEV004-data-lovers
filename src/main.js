323700ca67125f83027ceb352539f3d941289036
Maria Garcia <licgarcia_psic@hotmail.com>
2023-02-17 08:37:18 -0600

    Commit prueba Mari

--- src/main.js
+++ src/main.js
@@ -5,22 +5,22 @@ import datos from './data/pokemon/pokemon.js';
 const arregloPokemon = datos.pokemon.slice(0,18)
 
 const vistaPokemon = document.getElementById('vistaPokemon')
- function mostrarTarjetas(arregloPokemon){
+function mostrarTarjetas(arregloPokemon){
     console.log(arregloPokemon);
     for(let i = 0; i < arregloPokemon.length; i++){
-     vistaPokemon.innerHTML += `<article>
-     <h1>${arregloPokemon[i].name}</h1> 
-     <h2>Tipo:</h2>        
-     <h3>${arregloPokemon[i].type}<h3/>
-     <img src="${arregloPokemon[i].img}" alt="">
-     <h4>Resistencia:</h4>
-     <h5>${arregloPokemon[i].resistant}</h5>
-     <h6>${arregloPokemon[i].num}</h6>
+    vistaPokemon.innerHTML += `<article>
+    <h1>${arregloPokemon[i].name}</h1> 
+    <h2>Tipo:</h2>        
+    <h3>${arregloPokemon[i].type}<h3/>
+    <img src="${arregloPokemon[i].img}" alt="">
+    <h4>Resistencia:</h4>
+    <h5>${arregloPokemon[i].resistant}</h5>
+    <h6>${arregloPokemon[i].num}</h6>
    </article>` // sugar sintax
 
-     /*  const h1 = document.createElement('h1')
-       h1.textContent = arregloPokemon[i].name
-       vistaPokemon.appendChild(h1)*/
+    /*  const h1 = document.createElement('h1')
+      h1.textContent = arregloPokemon[i].name
+      vistaPokemon.appendChild(h1)*/
     }
     
     };
