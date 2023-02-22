f4265d87e53323c684b7e02af44ad98b262fa743
Maria Garcia <licgarcia_psic@hotmail.com>
2023-02-22 11:32:06 -0600

    commit prueba mari

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-import datos from './data/pokemon/pokemon.js';
-const arregloPokemon = datos.pokemon
+import datos from "./data/pokemon/pokemon.js";
+const arregloPokemon = datos.pokemon;
 
-const vistaPokemon = document.getElementById('vistaPokemon')
+const vistaPokemon = document.getElementById("vistaPokemon");
 function mostrarTarjetas(arregloPokemon) {
   for (let i = 0; i < arregloPokemon.length; i++) {
     vistaPokemon.innerHTML += `<article>
-     <h1>${arregloPokemon[i].name}</h1> 
-     <h2>Tipo:</h2>        
-     <h3>${arregloPokemon[i].type}<h3/>
-     <img src="${arregloPokemon[i].img}" alt="">
-     <h4>Resistencia:</h4>
-     <h5>${arregloPokemon[i].resistant}</h5>
-     <h6>${arregloPokemon[i].num}</h6>
-   </article>`
+    <h1>${arregloPokemon[i].name}</h1> 
+    <h2>Tipo:</h2>        
+    <h3>${arregloPokemon[i].type}<h3/>
+    <img src="${arregloPokemon[i].img}" alt="">
+    <h4>Resistencia:</h4>
+    <h5>${arregloPokemon[i].resistant}</h5>
+    <h6>${arregloPokemon[i].num}</h6>
+  </article>`;
   }
-};
-mostrarTarjetas(arregloPokemon)
+}
+mostrarTarjetas(arregloPokemon);
